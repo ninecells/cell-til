08707af2ff2abe43886c89795ddc2888e5aaa968
koojunho <koojh@9cells.com>
2016-03-01 07:39:00 +0900

    TIL 기본 routes 연결

---
+++ Http/Controllers/TilPostController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NineCells\Til\Http\Controllers;
+
+class TilPostController extends Controller
+{
+    public function GET_index()
+    {
+        return "TIL";
+    }
+}
--- Http/routes.php
+++ Http/routes.php
@@ -1,1 +1,12 @@
 <?php
+
+Route::group(['middleware' => ['web']], function () {
+
+    Route::group(['namespace' => 'NineCells\Til\Http\Controllers'], function() {
+
+        Route::group(['prefix' => 'til'], function() {
+
+            Route::get('/', 'TilPostController@GET_index');
+        });
+    });
+});
