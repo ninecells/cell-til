6938670a022d840edf80a97cfd66cd43206658e6
koojunho <koojh@9cells.com>
2016-03-02 00:57:26 +0900

    테이블 추가

--- database/migrations/2016_03_01_072109_create_til_tables.php
+++ database/migrations/2016_03_01_072109_create_til_tables.php
@@ -5,23 +5,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateTilTables extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        //
+        Schema::create('til_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('writer_id')->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('til_posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->index();
+            $table->string('title');
+            $table->longText('content');
+            $table->integer('writer_id')->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        //
+        Schema::drop('til_categories');
+        Schema::drop('til_posts');
     }
 }
