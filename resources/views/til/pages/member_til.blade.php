923c2d895bbc27febd4f3aef199e330537a8e8b9
koojunho <koojh@9cells.com>
2016-03-16 19:48:25 +0900

    Member tab partial view 를 include 시에 member_id 를 넘기게 변경
    - 내부적으로 $member 를 얻기위해 쿼리를 하는 형태로 변경
    - 인자로 넘어오지 않은 $member 를 사용하고 있었는데 대부분의 member tab 을 생성하는 view 가 $member 를 만들고 있어서 우연히 문제가 없던 것
    - @include 에서 'member_id' 라는 key 가 필요하다는 것을 사용자가 알고있어야 하는게 마음에 안들지만 아직 더 좋은 방법이 있는지 모르겠음

--- resources/views/til/pages/member_til.blade.php
+++ resources/views/til/pages/member_til.blade.php
@@ -1,16 +1,10 @@
-<?php
-use App\User;
-
-$member = User::find($member_id);
-?>
-
 @extends('ncells::jumbotron.app')
 
 @section('content')
 <div class="row">
     <div class="col-lg-12">
         <div>
-            @include('ncells::auth.parts.member_tab', ['member_id' => $member->id, 'tabitem_key' => 'til'])
+            @include('ncells::auth.parts.member_tab', ['member_id' => $member_id, 'tabitem_key' => 'til'])
             <div class="tab-content">
                 <br/>
                 <div role="tabpanel" class="tab-pane active">
